0f19b99f809cf29120c5a1d193880d96aaec8a8c
Jon Ward <jon.ward@vialer.com>
2021-10-30 21:26:12 +1300

    remove old config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      "sans": "Kanit, Tahoma, Geneva, Verdana, sans-serif"
+      "sans": "Rajdhani, Tahoma, Geneva, Verdana, sans-serif"
     },
     extend: {
       screens: {
         'xs':'376px'  
       },
-      colors: {
-        rose: { 
-          DEFAULT: '#FA3B4C', 
-          light: '#FA616A'
-        },
-        midnight: '#012639',
-        dusk: '#2F647F',
-        smoke: '#ECF8FE',
-      }
     },
   },
   variants: {
